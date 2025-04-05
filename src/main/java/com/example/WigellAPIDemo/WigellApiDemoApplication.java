ff50129928b517cf7176be588de084d09777b5cf
Erik Edman <61149651+ErikEdman1996@users.noreply.github.com>
2025-04-05 13:23:45 +0200

    First commit

---
+++ src/main/java/com/example/WigellAPIDemo/WigellApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.WigellAPIDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WigellAPIDemo/WigellApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WigellAPIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
